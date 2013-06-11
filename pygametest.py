b797c860381ca226c4947b77d1d8d7a4e8a26446
John Caine <jonathan.caine@btinternet.com>
2013-06-11 22:18:33 +0100

    Basic window that stays open for 20 seconds then exits cleanly

---
+++ pygametest.py
@@ -0,0 +1,17 @@
+import sys, pygame, time
+
+pygame.init()
+
+size = width, height = 320, 240
+screen = pygame.display.set_mode(size)
+
+pygame.display.flip()
+
+a = 0
+
+while a<50:
+	
+	a = a + 1
+	time.sleep(1)
+	
+sys.exit(1)
