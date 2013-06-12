680df94f70587b245e68f7c68ecbcd66a600876a
John Caine <jonathan.caine@btinternet.com>
2013-06-12 22:58:24 +0100

    Now tile the background automatically
    background is tiled with the same image horizontally, until it goes
    beyond the left edge of the window, then the next row vertically down is
    tiled etc, until it goes beyond the bottom edge of the window.
    
    Game now runs in infinite while loop waiting on exit .

--- pygametest.py
+++ pygametest.py
@@ -9,25 +9,29 @@ black = (0, 0, 0)
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
 
-square = pygame.image.load('grass.jpg').convert()
-#square2 = pygame.image.load('grass.jpg').convert()
-squarerect = square.get_rect()
-square2rect = square.get_rect().move(250, 0)
+grass = pygame.image.load('grass.jpg').convert()
 
 screen.fill(black)
-screen.blit(square, squarerect)
-screen.blit(square, square2rect)
+grassrect = grass.get_rect()
+xtracker = 0
+ytracker = 0
 
+while xtracker < width or ytracker < height:
+	screen.blit(grass, grassrect)
+	
+	if ytracker > height:
+		break
+	elif xtracker > width:
+		xtracker = 0
+		ytracker = ytracker + grass.get_height()
+		grassrect = grass.get_rect().move(xtracker, ytracker)		
+	else:
+		xtracker = xtracker + grass.get_width()
+		grassrect = grass.get_rect().move(xtracker, ytracker)
+			
 pygame.display.flip()
 
-a = 0
-
-while a<50:
+while 1:
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
-	
-	a = a + 1
-	time.sleep(1)
-	
-sys.exit(1)
