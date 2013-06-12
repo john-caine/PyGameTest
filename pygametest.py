f605af991c33ae3be4e72d92851d9d0d75e94be3
John Caine <jonathan.caine@btinternet.com>
2013-06-12 21:39:18 +0100

    can load same image (surface) onto multiple screen locations
    also added event handling for pygame.quit() calls

--- pygametest.py
+++ pygametest.py
@@ -1,15 +1,31 @@
-import sys, pygame, time
+import sys
+import pygame
+import time
 
 pygame.init()
 
-size = width, height = 320, 240
+black = (0, 0, 0)
+
+size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
+
+square = pygame.image.load('grass.jpg').convert()
+#square2 = pygame.image.load('grass.jpg').convert()
+squarerect = square.get_rect()
+square2rect = square.get_rect().move(250, 0)
+
+screen.fill(black)
+screen.blit(square, squarerect)
+screen.blit(square, square2rect)
 
 pygame.display.flip()
 
 a = 0
 
 while a<50:
+	
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
 	
 	a = a + 1
 	time.sleep(1)
