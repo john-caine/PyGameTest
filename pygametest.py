5c9e911979b78d73da29471a2cd4056dff7a8385
John Caine <jonathan.caine@btinternet.com>
2013-06-13 23:04:45 +0100

    Add GameObject base class, and added Player class
    Changed Tile and Player to come from base class GameObject, which is
    derived from pygame.sprite.Sprite. Added a title sprite group to Map
    class.
    Implemented player movement with key bindings

--- pygametest.py
+++ pygametest.py
@@ -20,32 +20,58 @@ class TiledSurface(pygame.Surface):
       for y in range(0,rows ):
         self.blit(image, (x * image_width, y * image_height))
 
-tile_size = (32,32)
+class GameObject(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    super(GameObject, self).__init__()
+    self.map = { 'x': x, 'y': y }
 
-class Tile(pygame.Surface):
+  def set_rect(self):
+    self.rect = self.image.get_rect().move(
+      tile_size[0] * self.map['x'],
+      tile_size[1] * self.map['y'])
+
+class Tile(GameObject):
   def __init__(self, image_name, x, y):
-    super(Tile, self).__init__(tile_size)
-    image = pygame.image.load(image_name).convert()
-    self.blit(image, (0, 0))
-    self.x = x
-    self.y = y
-    
-  def draw(self, surface):
-    surface.blit(self, (self.x * tile_size[0], self.y * tile_size[1]))
+    super(Tile, self).__init__(x, y)
+    self.image = pygame.image.load(image_name).convert()
+    self.set_rect()
 
 class Map:
   def __init__(self, rows, columns):
     self.rows = rows
     self.columns = columns
-    self.tiles = []
+    self.tiles = pygame.sprite.Group()
     for x in range(0, self.columns):
       for y in range(0, self.rows):
-        self.tiles.append(Tile(random.choice(['grass.jpg', 'rock.jpg', 'water.jpg']) , x, y))
+        self.tiles.add(Tile(random.choice(['grass.jpg', 'rock.jpg', 'water.jpg']) , x, y))
 
   def draw(self, surface):
-    for tile in self.tiles:
-      tile.draw(surface)
+    self.tiles.draw(surface)
 
+class Player(GameObject):
+  def __init__(self, x, y):
+    super(Player, self).__init__(x, y)
+    self.image = pygame.image.load('player.png').convert()
+    self.set_rect()
+      
+  def down(self):
+    self.map['y'] += 1
+    self.set_rect()
+
+  def up(self):
+    self.map['y'] -= 1
+    self.set_rect()
+    
+  def left(self):
+    self.map['x'] -= 1
+    self.set_rect()
+
+  def right(self):
+    self.map['x'] += 1
+    self.set_rect()
+  
+tile_size = (32,32)
+      
 if __name__ == "__main__":
   pygame.init()
 
@@ -54,12 +80,26 @@ if __name__ == "__main__":
   screen = pygame.display.set_mode(size)
   
   map = Map(columns, rows)
+  player = Player(0, 0)
+  player_group = pygame.sprite.Group()
+  player_group.add(player)
 
   while True:
     
     for event in pygame.event.get():
       if event.type == pygame.QUIT: sys.exit()
     
+      elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_DOWN:
+          player.down()
+        elif event.key == pygame.K_UP:
+          player.up()
+        elif event.key == pygame.K_LEFT:
+          player.left()
+        elif event.key == pygame.K_RIGHT:
+          player.right()
+    
     map.draw(screen)
+    player_group.draw(screen)
     
     pygame.display.flip()
