c02ae8dd6804d7ee6e246e14ba1822628b3e808a
John Caine <jonathan.caine@btinternet.com>
2013-06-13 20:00:32 +0100

    Refactor tile_surface into a subclass on pygame.Surface

--- pygametest.py
+++ pygametest.py
@@ -3,21 +3,21 @@ import pygame
 import time
 import math
 
-def tiled_surface((width, height), imagename):
-  surface = pygame.Surface((width, height))
-  image = pygame.image.load(imagename).convert()
-  
-  image_height = image.get_height()
-  image_width = image.get_width()
-  
-  columns = int(math.ceil(float(height) / float(image_height)))
-  rows = int(math.ceil(float(width) / float(image_width)))
+class TiledSurface(pygame.Surface):
+  def __init__(self, (width, height), image_name):
+    super(TiledSurface, self).__init__((width, height))
+    
+    image = pygame.image.load(image_name).convert()
+    
+    image_height = image.get_height()
+    image_width = image.get_width()
+    
+    columns = int(math.ceil(float(height) / float(image_height)))
+    rows = int(math.ceil(float(width) / float(image_width)))
 
-  for x in range(0, columns):
-    for y in range(0,rows ):
-      surface.blit(image, (x * image_width, y * image_height))
-      
-  return surface
+    for x in range(0, columns):
+      for y in range(0,rows ):
+        self.blit(image, (x * image_width, y * image_height))
 
 if __name__ == "__main__":
   pygame.init()
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
 
   screen = pygame.display.set_mode(size)
-  background = tiled_surface(size, 'grass.jpg')
+  background = TiledSurface(size, 'grass.jpg')
 
   screen.blit(background, (0,0))
   pygame.display.flip()
