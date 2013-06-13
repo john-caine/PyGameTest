4979d2d9e2b85a7f4c4053029272c618ddc9caad
John Caine <jonathan.caine@btinternet.com>
2013-06-13 19:59:24 +0100

    Refactor tile_screen into tile_surface

--- pygametest.py
+++ pygametest.py
@@ -1,43 +1,39 @@
 import sys
 import pygame
 import time
+import math
 
-def tile_screen(imagename): 
-	image = pygame.image.load(imagename).convert()
-	
-	imagerect = image.get_rect()
-	xtracker = 0
-	ytracker = 0
+def tiled_surface((width, height), imagename):
+  surface = pygame.Surface((width, height))
+  image = pygame.image.load(imagename).convert()
+  
+  image_height = image.get_height()
+  image_width = image.get_width()
+  
+  columns = int(math.ceil(float(height) / float(image_height)))
+  rows = int(math.ceil(float(width) / float(image_width)))
 
-	while xtracker < width or ytracker < height:
-		screen.blit(image, imagerect)
-	
-		if ytracker > height:
-			break
-		elif xtracker > width:
-			xtracker = 0
-			ytracker = ytracker + image.get_height()
-			imagerect = image.get_rect().move(xtracker, ytracker)		
-		else:
-			xtracker = xtracker + image.get_width()
-			imagerect = image.get_rect().move(xtracker, ytracker)
-
+  for x in range(0, columns):
+    for y in range(0,rows ):
+      surface.blit(image, (x * image_width, y * image_height))
+      
+  return surface
 
 if __name__ == "__main__":
-	pygame.init()
+  pygame.init()
 
-	black = (0, 0, 0)
-	size = width, height = 500, 500
-				
-	screen = pygame.display.set_mode(size)
-	screen.fill(black)
+  black = (0, 0, 0)
+  size = width, height = 500, 500
 
-	tile_screen('grass.jpg')
-				
-	pygame.display.flip()
 
-	while 1:
-		
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: sys.exit()
-		
+  screen = pygame.display.set_mode(size)
+  background = tiled_surface(size, 'grass.jpg')
+
+  screen.blit(background, (0,0))
+  pygame.display.flip()
+
+  while 1:
+    
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT: sys.exit()
+    
